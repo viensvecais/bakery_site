5017bbd12ef4543e97440a3c59a447e7d832020c
Mārtiņš Muša <martins@martins-musa.com>
2018-05-02 22:16:00 +0300

    Progress 02.05.2018 - sakārtoju parauga faila platumu, pārkārtoju navigāciju un hero daļu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 
-import '../css/main.css';
-import '../css/normalize.css';
-import '../assets/sass/main.sass';
-import './plugins.js';
-import './functions.js';
+
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
+import './plugins.js';
+import './functions.js';
+import '../css/main.css';
+import '../css/normalize.css';
+import '../assets/sass/main.sass';
+
